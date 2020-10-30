972bce847afeba6e69f82ecc1cd1838bfc4d94ad
Sony A P <sonyap@Sonys-MacBook-Air.local>
2020-10-30 21:42:31 +0530

    Added doc string to the methods.

--- impact_analytics/binary_tree_node_sum/binary_tree_operation.py
+++ impact_analytics/binary_tree_node_sum/binary_tree_operation.py
@@ -11,13 +11,22 @@ class BinaryTreeSum:
         self.root = None
     
     def BinaryTreeNodeDepthSum(self, root):
+        """Method that calculates sum of depths of all nodes of the given tree.
+        @root: Start node of the Binary tree."""
         return self.__binary_tree_node_sum(root)
     
     def build_binary_tree(self, root_id, json_data):
+        """Method that builds the tree with the given JSON.
+        @root_id: ID of the start node.
+        @json_data: Dictionary of nodes."""
         self.root = self.__build_binary_tree(self.root, root_id, json_data)
         return self.root
     
     def __build_binary_tree(self, root, node_id, json_data):
+        """Private methods that internally builds the Binary tree with given JSON.
+        @root: Object of BinaryTree class, means any node in the Binary Tree.
+        @node_id: ID of the root.
+        @json_data: Dictionary of nodes."""
         new_node = BinaryTree(value=json_data[node_id]["value"], left=None, right=None)
         if json_data[node_id]["left"] != None:
             new_node.left = self.__build_binary_tree(new_node, json_data[node_id]["left"], json_data)
@@ -26,6 +35,9 @@ class BinaryTreeSum:
         return new_node
     
     def __binary_tree_node_sum(self, root, depth=0):
+        """Private method that internally calculates the sum of depths of all nodes.
+        @root: Start node of the Binary Tree.
+        @depth: Depth of the current node. For start node, depth will be 0."""
         if root == None:
             return self.node_sum
         self.node_sum += depth
--- impact_analytics/binary_tree_node_sum/main_binary_tree_node_sum.py
+++ impact_analytics/binary_tree_node_sum/main_binary_tree_node_sum.py
@@ -7,12 +7,16 @@ from binary_tree_operation import BinaryTreeSum
 class Parser:
     
     def get_json_data(self):
+        """Method to get the json."""
         args = self.__command_line_reader()
         json_data = self.__validate_input_and_get_json(args)
         return self.__json_parser(json_data)
 
     # Private methods
     def __json_parser(self, json_data):
+        """Method that parses JSON and gets main node(root node)
+        and Dictionary of nodes.
+        @json_data: JSON object."""
         if not json_data:
             print("Error: JSON cannot be empty.")
             exit(1)
@@ -23,6 +27,8 @@ class Parser:
         return root_node, node_map
     
     def __command_line_reader(self):
+        """Method that gets input as command line argument
+        and returns the parser object."""
         parser = argparse.ArgumentParser()
         parser.add_argument('-f', '--file_name', help="Provide the path to a JSON file",
             type=str, required=False
@@ -34,6 +40,9 @@ class Parser:
         return args
     
     def __validate_input_and_get_json(self, args):
+        """Method to validate the input read from CLI
+        and reads the JSON object.
+        @args: ArgumentParser object"""
         if not args.file_name and not args.string:
             print("Error: One of file name or string is mandatory")
             exit(1)
